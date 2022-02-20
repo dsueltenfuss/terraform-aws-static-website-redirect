67d6ee248f1f5bbf15d26126bdff1f4206370db7
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-02-20 15:50:28 -0500

    fix: use an empty array instead of a null

--- acm.tf
+++ acm.tf
@@ -9,7 +9,7 @@ resource "aws_acm_certificate" "certificate" {
 
 resource "aws_route53_record" "route_53_validation_record" {
   for_each = {
-    for dvo in try(aws_acm_certificate.certificate[0].domain_validation_options, null) : dvo.domain_name => {
+    for dvo in try(aws_acm_certificate.certificate[0].domain_validation_options, []) : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type

164f8b69c8884d71012525a95fb4ae30754ab8ed
Dave Sueltenfuss <dsueltenfuss@fearless.tech>
2022-02-20 15:30:44 -0500

    fix: fix for acm validation

--- acm.tf
+++ acm.tf
@@ -8,25 +8,19 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "route_53_validation_record" {
-  for_each = {
-    for cert in aws_acm_certificate.certificate : cert.domain_name => {
       for_each = {
-        for dvo in cert.domain_validation_options : dvo.domain_name => {
+        for dvo in local.certificate.domain_validation_options : dvo.domain_name => {
           name   = dvo.resource_record_name
           record = dvo.resource_record_value
           type   = dvo.resource_record_type
         }
-      }
     }
 
-  }
-
-
   allow_overwrite = true
-  name            = each.value.for_each.name
-  records         = [each.value.for_each.record]
+  name            = each.value.name
+  records         = [each.value.record]
   ttl             = 60
-  type            = each.value.for_each.type
+  type            = each.value.type
   zone_id         = data.aws_route53_zone.validation_zone.zone_id
 }
 
